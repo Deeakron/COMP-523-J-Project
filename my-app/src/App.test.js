d56fa2c5603ce78a283054f93d622011356da7e6
CGTNathan <cgtnathan@gmail.com>
2020-10-30 18:25:55 -0400

    Added some tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-
+import * as spreadsheet from "./spreadsheet";
+/*
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElemeqnt = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+*/
+
+const nullSheetId = null;
+test('GetTitle - Null', async () => {
+  expect(await spreadsheet.getTitle(nullSheetId)).not.toBe('Test');
+});
+
+const invalidSheetId = 'a'
+test('GetTitle - Invalid', async () => {
+  expect(await spreadsheet.getTitle(invalidSheetId)).not.toBe('Test');
+});
+
+const validSheetId='194iz66ka28ejiDV6_iHtGyV7V6gK8QSl9eFb8cMC1IM'
+test('GetTitle - Valid', async () => {
+  expect(await spreadsheet.getTitle(validSheetId)).toBe('Test');
 });
